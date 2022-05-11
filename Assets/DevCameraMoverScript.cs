849800b7b06afd3503fd207ad75cbf454ff2b35c
Pepsicle <48803453+Pepsicle@users.noreply.github.com>
2022-05-11 14:39:37 +0200

    First rendition of map

---
+++ Assets/DevCameraMoverScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class DevCameraMoverScript : MonoBehaviour
+{
+    public CinemachineDollyCart dollyScript;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
