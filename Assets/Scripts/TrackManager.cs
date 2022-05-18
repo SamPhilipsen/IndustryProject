b5062447eae8711f66c10afdc655433772d9b1ce
Sam Philipsen <i467404@fhict.nl>
2022-05-18 09:41:41 +0200

    Track switcher works for hard coded alternative track. Next up is to make it more modular so you can just throw in a track and have it work

--- Assets/Scripts/TrackManager.cs
+++ Assets/Scripts/TrackManager.cs
@@ -10,9 +10,13 @@ public class TrackManager : MonoBehaviour
     [SerializeField] private CinemachineDollyCart cart;
     [SerializeField] private CinemachineSmoothPath[] alternativeTracks;
     private CinemachineSmoothPath track;
+
     private CinemachineSmoothPath.Waypoint[] activePath;
     private CinemachineSmoothPath.Waypoint[] originalPath;
+    private CinemachineSmoothPath.Waypoint[] activeAltPath;
+
     private bool onAltTrack;
+    private int currentWaypoint;
     
     void Start()
     {
@@ -20,51 +24,48 @@ public class TrackManager : MonoBehaviour
         originalPath = track.m_Waypoints;
         activePath = originalPath;
         onAltTrack = false;
+        currentWaypoint = 0;
     }
 
     void Update()
     {
         track.m_Waypoints = activePath;
-        CheckIfCartReachedWaypoint();
-        CheckIfCartEnd();
-    }
-
-    void CheckIfCartReachedWaypoint()
-    {
-        foreach(CinemachineSmoothPath path in alternativeTracks)
+        
+        currentWaypoint = (int)Mathf.Floor(cart.m_Position);
+        if(cart.m_Position >= currentWaypoint + 0.95)
         {
-            if(Vector3.Distance(transform.TransformPoint(path.m_Waypoints[0].position), cart.transform.position) <= 1)
+            if (onAltTrack) CheckIfCartEnd();
+
+            //should have an IF check to check if the player wants to go on the alternate track.
+            //Currently does it automatically.
+            if (!onAltTrack)
             {
-                if(!onAltTrack)
-                    AAAAAAAAAAAAH();
+                foreach (CinemachineSmoothPath path in alternativeTracks)
+                {
+                    if (activePath[currentWaypoint + 1].position == path.m_Waypoints[0].position) AAAAAAAAAAAAH(path);
+                }
             }
         }
     }
+
     void CheckIfCartEnd() { 
-        if(cart.transform.position == transform.TransformPoint(activePath[activePath.Length - 1].position))
+        if(Mathf.Round(cart.m_Position) == 6)
         {
-            activePath = originalPath;
             onAltTrack = false;
         }
     }
 
-    void AAAAAAAAAAAAH() {
+    void AAAAAAAAAAAAH(CinemachineSmoothPath switchingPath) {
         onAltTrack = true;
         int firstPointIndex = 0; int endPointIndex = 0;
 
         for(int i = 0; i < activePath.Length; i++)
         {
-            if (activePath[i].position == alternativeTracks[0].m_Waypoints[0].position) firstPointIndex = i;
-            if (activePath[i].position == alternativeTracks[0].m_Waypoints[alternativeTracks[0].m_Waypoints.Length - 1].position) endPointIndex = i;
+            if (activePath[i].position == switchingPath.m_Waypoints[0].position) firstPointIndex = i;
+            if (activePath[i].position == switchingPath.m_Waypoints[switchingPath.m_Waypoints.Length - 1].position) endPointIndex = i;
         }
         Debug.Log("First: " + firstPointIndex);
         Debug.Log("End: " + endPointIndex);
-
-        // Debug.Log(alternativeTracks[0].m_Waypoints[0].position);
-        //Debug.Log(originalPath[1].position);
-
-        /*ArrayUtility.RemoveAt(ref alternativeTracks[0].m_Waypoints, 0);
-        ArrayUtility.RemoveAt(ref alternativeTracks[0].m_Waypoints, alternativeTracks[0].m_Waypoints.Length - 1);*/
 
         CinemachineSmoothPath.Waypoint[] temp = new CinemachineSmoothPath.Waypoint[0];
 
@@ -76,11 +77,12 @@ public class TrackManager : MonoBehaviour
             }
             if (i == firstPointIndex)
             {
-                ArrayUtility.AddRange(ref temp, alternativeTracks[0].m_Waypoints);
+                ArrayUtility.AddRange(ref temp, switchingPath.m_Waypoints);
             }
             if (i != firstPointIndex && i != endPointIndex) ArrayUtility.Add(ref temp, activePath[i]);
         }
 
+        activeAltPath = switchingPath.m_Waypoints;
         activePath = temp;
 
     }
