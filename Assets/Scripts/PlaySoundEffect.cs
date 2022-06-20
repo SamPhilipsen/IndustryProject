e9e617f7751d38f10ee885dbe1a9b4cd52d0de53
Brian Hutjens <57947324+YZBrian@users.noreply.github.com>
2022-06-20 10:51:37 +0200

    Menu Music
    Implemented both the menu music and the on-click SFX.

---
+++ Assets/Scripts/PlaySoundEffect.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundEffect : MonoBehaviour
+{
+
+    public AudioSource soundPlayer;
+
+    public void playSoundEffect()
+    {
+        soundPlayer.Play();
+    }
+}
